8bddb78b3994d600011edf5ec2b0e356e7a2408d
Victor Adeshile <13077450+davik4life@users.noreply.github.com>
2020-12-16 23:21:59 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <h1 class="title">Weather App</h1>
+  <input class="searchbox" @click="searching" placeholder="Weather Search..." />
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      searching() {},
+    };
+  },
+};
+</script>
+
+<style>
+.title {
+  display: flex;
+  justify-content: center;
+}
+.searchbox {
+  display: flex;
+  justify-content: center;
+  /* align-items: center; */
+  /* box */
+}
+</style>
